0eb0169be5f56cd93bbbffea341f5b7a70d1640c
pandyamarut <pandyamarut@gmail.com>
2019-04-19 16:29:01 +0530

    initial commit.

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'uEzqP9oilIgVem129oNcSgYDz',
+  consumer_secret: 'Tur1XWySMptb26q9PgE7GfcNSTeHzkphVaNH5nYHtNeW1hqEXK',
+  access_token_key: '850623554651639809-APBzXrWIcQQDz3kR7Du1PMguPeUWDTg',
+  access_token_secret: 'Oty3mSMLv8qsHX95bc1OtmJ22QRx14ozZvBMOrsTHtXHn'
+}
